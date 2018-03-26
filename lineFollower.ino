fe57b00cb4c03eb86bb9fe55b2aff5775add6c7c
Valentin Marginean <valentin.marginean@best.eu.org>
2018-03-26 23:09:55 +0300

    Added Infrared Sensor Code

--- lineFollower.ino
+++ lineFollower.ino
@@ -6,13 +6,13 @@
 #define SPEED_MAX 255
 #define SPEED_MIN 0
 
-  int In3 = 8;
-  int In4 = 6;
-  int leftMotorPin = 5;
-
-  int In1 = 4;
-  int In2 = 2;
-  int RightMotorPin = 3;
+//  int In3 = 8;
+//  int In4 = 6;
+//  int leftMotorPin = 5;
+//
+//  int In1 = 4;
+//  int In2 = 2;
+//  int RightMotorPin = 3;
 
 /**SensorPins : left to right**/
 int sensorPin[SENSOR_COUNT] = {A1, A2, A3, A4, A5};
@@ -20,43 +20,33 @@ int sensorVal[SENSOR_COUNT];
 int ledPin[SENSOR_COUNT] = {2,3,4,5,6};
 int leftLedPin = 8;
 int rightLedPin = 9;
+int lastTime;
+int currentTime;
+boolean(isOn);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-//  for(int i = 0; i < SENSOR_COUNT; i++) {
-//    pinMode(ledPin[i],OUTPUT);
-//  }
-//  pinMode(leftLedPin, OUTPUT);
-//  pinMode(rightLedPin, OUTPUT);
-//  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  isOn = false;
 }
 
 void loop() {
-
-  // read sensors data
-  for(int i = 0; i < SENSOR_COUNT; i++) {
-    sensorVal[i] = analogRead(sensorPin[i]);
-    Serial.print(sensorVal[i]); 
-    Serial.print(","); 
+  powerSwitch();
+  if(isOn) {
+    // read sensors data
+    for(int i = 0; i < SENSOR_COUNT; i++) {
+      sensorVal[i] = analogRead(sensorPin[i]);
+      Serial.print(sensorVal[i]); 
+      Serial.print(","); 
+    }
+    Serial.println();
+  //  blinker();
+    int score = getScore();
+    displayLeds();
+    showDirection(score);
+  //  steer(score);
   }
-  Serial.println();
-//  blinker();
-  int score = getScore();
-  displayLeds();
-  showDirection(score);
-//  steer(score);
-  
-//  Serial.println(analogRead(A1)); 
-//  Serial.print(","); 
-//  Serial.print(analogRead(A2));
-//  Serial.print(","); 
-//  Serial.print(analogRead(A3));
-//  Serial.print(","); 
-//  Serial.print(analogRead(A4));
-//  Serial.print(","); 
-//  Serial.print(analogRead(A5));
-//  Serial.print("\n"); 
 }
 
 void displayLeds(){
@@ -66,6 +56,20 @@ void displayLeds(){
     else
       digitalWrite(ledPin[i], LOW);
   }
+}
+
+boolean powerSwitch() {
+  int remoteSensorVal = digitalRead(remoteSensorPin);
+  //Filter out random noise
+  if(remoteSensorVal < 1) {
+    currentTime = millis();
+    if(currentTime - lastTime > 150) {
+      lastTime = currentTime;
+      isOn = !isOn;
+      return true;
+    }
+  }
+  return false;
 }
 
 int getScore(){
@@ -79,16 +83,16 @@ void blinker(){
   delay(500);
 }
 
-void steer(int score){
-  double normalizedScore = (double) score / 1500;
-  analogWrite(leftMotorPin, SPEED_MAX);
-  analogWrite(rightMotorPin, SPEED_MAX);
-  if(score > DIRECTION_THRESHOLD){
-    analogWrite(rightMotorPin, SPEED_MAX * (1 - normalizedScore));
-  }else if(score < -DIRECTION_THRESHOLD){
-    analogWrite(leftMotorPin, SPEED_MAX * (1 + normalizedScore));
-  }
-}
+//void steer(int score){
+//  double normalizedScore = (double) score / 1500;
+//  analogWrite(leftMotorPin, SPEED_MAX);
+//  analogWrite(rightMotorPin, SPEED_MAX);
+//  if(score > DIRECTION_THRESHOLD){
+//    analogWrite(rightMotorPin, SPEED_MAX * (1 - normalizedScore));
+//  }else if(score < -DIRECTION_THRESHOLD){
+//    analogWrite(leftMotorPin, SPEED_MAX * (1 + normalizedScore));
+//  }
+//}
 
 void showDirection(int score){
   digitalWrite(leftLedPin, LOW);
